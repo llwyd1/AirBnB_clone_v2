c125e5d69c7edcc04761c3103805513ec12aa5a6
Lloyd <llwydmodes@gmail.com>
2024-06-26 22:56:22 +0200

    Added task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Prepares a MySQL server for the Airbnb clone
+-- Creates a database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Creates a user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grants a user previleges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grants a user SELECT previlege on perfomance_schema
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
