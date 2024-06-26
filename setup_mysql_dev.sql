2ebd4e52a65b7481b24e24fa874b7a905d3fdbfa
Lloyd <llwydmodes@gmail.com>
2024-06-26 22:45:03 +0200

    Added task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Prepares a MySQL server for the AirBnB clone
+-- Creates a database
+CREATE DATABASE IF NOT EXISTS hbnb_dev;
+-- creates a user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grants a user all privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- grants a user SELECT privilege on performance_schema database;
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
