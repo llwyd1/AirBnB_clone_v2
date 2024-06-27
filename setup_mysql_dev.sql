a3734b14c8196dbd9d76b9738f96f1e5508f8966
Lloyd <llwydmodes@gmail.com>
2024-06-27 14:25:05 +0200

    Corrected task 3 and task 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,4 +6,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grants a user all privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- grants a user SELECT privilege on performance_schema database
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,4 +6,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grants a user previleges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grants a user SELECT previlege on perfomance_schema
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
